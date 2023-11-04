2c91863934a044e768999870c8d9add2763e81a0
rrushman02 <144175872+rrushman02@users.noreply.github.com>
2023-11-04 11:08:16 -0500

    Remove comments and extra content

--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
-
 
 import numpy as np
 import pandas as pd
@@ -11,55 +9,29 @@ import matplotlib.pyplot as plt
 import calculator
 
 
-# In[5]:
-
-
 pip install -U selenium
 
-
-# In[18]:
 
 
 import scraper
 
 
-# In[19]:
-
 
 reviews = scraper.critic_responses
 
-
-# In[20]:
 
 
 review_words = calculator.createWordList(reviews, calculator.getWordsToRemove("Five Nights at Freddy's"))
 
 
-# In[21]:
-
-
-review_words
-
-
-# In[22]:
 
 
 review_words_df = pd.DataFrame(review_words, columns = ['Word'])
 
 
-# In[23]:
-
-
-review_words_df.head()
-
-
-# In[24]:
-
 
 word_counts = review_words_df.groupby(['Word'])
 
-
-# In[25]:
 
 
 plt.figure(figsize=(15, 10))
@@ -69,7 +41,6 @@ plt.xlabel("Words Used in Movie Reviews")
 plt.ylabel("Frequency of Words")
 
 
-# In[ ]:
 
 
 
