f540b3137a36ca185b26e30fd0f96fb9a00918ae
rosnMagar <roshan.thamagar@gmail.com>
2023-11-05 00:49:04 -0500

    gui integrated with main file

--- main.py
+++ main.py
@@ -2,27 +2,11 @@ from calculator import getWordsToRemove
 from calculator import createWordList
 from scraper import Scraper
 import visualizer
+import gui
 
-
-#TODO integrate this into a GUI
-movie_name = input("Enter the movie name: ")
-
-# setup the scraper
-
-# get reviews from 10 pages
-scraper = Scraper(movie_name, 10)
-scraper.initialize_driver("--ignore-certificate-error", "--incognito", "--headless")
-scraper.scrape()
-
-reviews = scraper.critic_responses
-title = scraper.movie_name 
-
-wordsToRemove = getWordsToRemove(title)
-
-wordList = createWordList(reviews, wordsToRemove)
-print(wordList)
-
-visualizer.review_visuals(title)
-
-input('Press ENTER to exit')
-
+#wordsToRemove = getWordsToRemove(title)
+#
+#wordList = createWordList(reviews, wordsToRemove)
+#print(wordList)
+#
+#visualizer.review_visuals(title)
